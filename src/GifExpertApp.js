2d1a055c9188468b96dab7f117e1b0e7b7d56a2e
Carlos Echeverria <carlos.echeverria_ext@mkdautomotive.com>
2020-09-19 12:12:11 -0400

    Add category component created

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import AddCategory from "./components/AddCategory";
 
 const GifExpertApp = () => {
+  //   const categories = ["dragon ball", "samurai x", "death note"];
+  const [categories, setCategories] = useState([
+    "dragon ball",
+    "samurai x",
+    "death note",
+  ]);
+
+  const handleAdd = () => {
+    // setCategories([...categories, "one punch"]);
+    setCategories((cats) => [...cats, "one punch"]);
+  };
+
   return (
     <>
       <h2>Gif Expert App</h2>
+      <AddCategory />
       <hr />
+      <button onClick={handleAdd}> Agregar </button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}> {category}</li>;
+        })}
+      </ol>
     </>
   );
 };
